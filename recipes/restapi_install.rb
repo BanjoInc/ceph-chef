ab7fc177f149c7f4637a234cdd09f2fb6b25ae3e
wei <weiwu1120@gmail.com>
2017-10-02 17:50:22 -0700

    Add cluster name to ceph-rest-api.service

--- recipes/restapi_install.rb
+++ recipes/restapi_install.rb
@@ -30,8 +30,7 @@ case node['platform_family']
 when 'rhel'
   # NOTE: We will be doing a PR on the main Ceph repo soon that does the systemd config for ceph-rest-api but
   # for now, this will create the required config.
-  cookbook_file '/etc/systemd/system/ceph-rest-api.service' do
-    source 'ceph-rest-api.service'
+  template '/etc/systemd/system/ceph-rest-api.service' do
     owner 'root'
     group 'root'
     mode '0644'
